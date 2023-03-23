fe7101023ffb301d244eaf35110074c5f5b6d30f
Raiene Lemos <raienelemos@outlook.com>
2023-03-23 16:29:43 -0300

    Finalizar de github_API.py

--- libpythonpro/github_API.py
+++ libpythonpro/github_API.py
@@ -10,5 +10,6 @@ def buscar_avatar(usuario):
 
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
-    return resp.json()['avatar_url']
+    return  \
+        ['avatar_url']
 
